6e8815dc1627e09e08db0d952d8ca8c01b93a019
sandikodev <33443311+sandikodev@users.noreply.github.com>
2023-06-11 10:12:22 +0000

    add shadcn-ui

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
+import {vitePreprocess} from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $components: "src/lib/components",
+      "$components/*": "src/lib/components/*"
+    }
+  }
 };
-
 export default config;
